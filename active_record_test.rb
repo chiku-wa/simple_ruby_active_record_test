e6b42b0b3cda759f4406ab406b626a4f9f1aa171
watson <chikuwa.wachi@gmail.com>
2020-02-03 22:17:56 +0900

    [change]Changed to read environment variables.

--- active_record_test.rb
+++ active_record_test.rb
@@ -1,10 +1,20 @@
+# ============================================================
+# Require処理
+# DBのテーブルをORマッピングするためのActiveRecord
 require "active_record"
 
+require "dotenv"
+Dotenv.load
+
+# ============================================================
+# モデルクラスの定義
 class User < ActiveRecord::Base
   # テーブルを明示したい場合は以下の通り
   # self.table_name = "users"
 end
 
+# ============================================================
+# メイン処理部
 ActiveRecord::Base.establish_connection(
   # ===== Setting for PostgreSQL
   # :adapter  => "postgresql",
@@ -15,7 +25,7 @@ ActiveRecord::Base.establish_connection(
 
   # ===== Setting for Sqlite3
   adapter: "sqlite3",
-  database: "foo",
+  database: ENV["DATABASE_NAME"],
 )
 
 users = User.all
